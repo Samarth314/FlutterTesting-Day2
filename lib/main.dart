678be3747929dcfd5cb5e6281358c0946dc21a6f
Samarth Shah <samarthandroid145@gmail.com>
2021-01-31 16:40:47 -0800

    day 4 commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    // main methodg
+    // main method
     MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.ctfassets.net/hrltx12pl8hq/4f6DfV5DbqaQUSw0uo0mWi/ff068ff5fc855601751499d694c0111a/shutterstock_376532611.jpg?fit=fill&w=800&h=300'),
+          ),
+        ),
       ),
     ),
   );
